03a709112a5a783752f26647a8045fbfec86fb45
pestevao <pestevao@student.42lisboa.com>
2023-10-28 17:12:03 +0100

    pong div

--- frontend/src/pages/Sidebar.tsx
+++ frontend/src/pages/Sidebar.tsx
@@ -10,33 +10,36 @@ import * as Exit from '../images/exit-icon.png';
 function Sidebar() {
 
   return (
-    <div className="sidebar">
-        <div className="logo">
-            <img className='ilogo' src={Logo.default} alt="Logo Pong" />
-        </div>
-        <div className="menu">
-            <div className="menu-item">
-                <img className='iplay' src={Play.default} alt="Play icon" />
-                <span>PLAY</span>
-            </div>
-            <div className="menu-item">
-                <img className='ileaderboard' src={Leaderboard.default} alt="Logo Pong" />
-                <span>LEADERBOARD</span>
-            </div>
-            <div className="menu-item">
-                <img className='ichat' src={Chat.default} alt="Chat icon" />
-                <span>CHAT</span>
-            </div>
-            <div className="menu-item">
-                <img className='iprofile' src={Profile.default} alt="Profile icon" />
-                <span>PROFILE</span>
-            </div>
-            <div className="menu-item">
-                <img className='iexit' src={Exit.default} alt="Exit icon" />
-                <span>EXIT</span>
-            </div>
-        </div>
-    </div>
+	<div className="sidebar">
+		<div className="logo">
+			<img className='ilogo' src={Logo.default} alt="Logo Pong" />
+		</div>
+		<div className="menu">
+			<div className="menu-item">
+				<img className='iplay' src={Play.default} alt="Play icon" />
+				<span>PLAY</span>
+			</div>
+			<div className="menu-item">
+				<img className='ileaderboard' src={Leaderboard.default} alt="Logo Pong" />
+				<span>LEADERBOARD</span>
+			</div>
+			<div className="menu-item">
+				<img className='ichat' src={Chat.default} alt="Chat icon" />
+				<span>CHAT</span>
+			</div>
+			<div className="menu-item">
+				<img className='iprofile' src={Profile.default} alt="Profile icon" />
+				<span>PROFILE</span>
+			</div>
+			<div className="menu-item">
+				<img className='iexit' src={Exit.default} alt="Exit icon" />
+				<span>EXIT</span>
+			</div>
+		</div>
+		<div = className="pong">
+			{/* pong */}
+		</div>
+	</div>
   );
 };
 
