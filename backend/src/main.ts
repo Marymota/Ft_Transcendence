d86a00f0ee471381ea15ab8abe3d49a1dc1ac8e9
António Abranches Pinto <antoniomariabranches@gmail.com>
2023-10-28 16:42:08 +0100

    litle changes to frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,15 +6,3 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-
-//   .catch(async (e) => {
-//     console.error(e);
-
-//     await prisma.$disconnect();
-
-//     process.exit(1);
-//   });
