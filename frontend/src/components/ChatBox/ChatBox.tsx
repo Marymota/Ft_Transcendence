2438539443ee8f40d743fcfe9f019310ede6a064
António Abranches Pinto <antoniomariabranches@gmail.com>
2023-10-30 11:16:13 +0000

    styled chat page

--- frontend/src/components/ChatBox/ChatBox.tsx
+++ frontend/src/components/ChatBox/ChatBox.tsx
@@ -3,7 +3,19 @@ import "./ChatBox.css";
 function ChatBox() {
   return (
     <>
-      <div className="box"></div>
+      <div className="chatBox">
+        <div className="chatGroups">
+          <input className="searchGroup" placeholder="Search..."></input>
+          <div className="group-names">
+            <div className="groupName group1">Paulo</div>
+            <div className="groupName group2">Antonio</div>
+            <div className="groupName group3">Simao</div>
+            <div className="groupName group4">Pedro</div>
+            <div className="groupName group5">Andre</div>
+          </div>
+        </div>
+        <div className="chatDisplay"></div>
+      </div>
     </>
   );
 }
--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -1,12 +1,15 @@
 import ChatBox from "../../components/ChatBox/ChatBox";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import "./Chat.css";
 
 const Chat: React.FC = () => {
   return (
     <>
       <div className="container-row">
         <Sidebar />
-        <ChatBox />
+        <div className="container-column">
+          <ChatBox />
+        </div>
       </div>
     </>
   );
