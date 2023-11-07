d90cdf227e9f7b72bad7aab43a6e4540fed71e7a
pestevao <pestevao@student.42lisboa.com>
2023-11-07 18:45:58 +0000

    game test

--- frontend/src/components/Game/Game.tsx
+++ frontend/src/components/Game/Game.tsx
@@ -1,15 +1,38 @@
 import "./Game.css";
 
+
+document.addEventListener('keydown', function(event) {
+	let rightplayer = document.getElementById('.rightplayer');
+
+	// Get the current position of the rightplayer.
+	if (rightplayer != null)
+	{
+			let style = window.getComputedStyle(rightplayer);
+			let top = parseInt(style.getPropertyValue('top'));
+
+			
+			// Update the position based on the key pressed.
+			switch (event.key) {
+					case 'ArrowUp':
+							rightplayer.style.top = `${top - 10}px`;
+							break;
+					case 'ArrowDown':
+							rightplayer.style.top = `${top + 10}px`;
+							break;
+				}
+	}
+});
+
 const Game: React.FC = () => {
-  return (
-    <div className="game">
-      <div className="pong">
-        <div className="leftplayer"></div>
-        <div className="rightplayer"></div>
-        <div className="ball"></div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="game">
+			<div className="pong">
+				<div className="leftplayer"></div>
+				<div className="rightplayer"></div>
+				<div className="ball"></div>
+			</div>
+		</div>
+	);
 };
 
 export default Game;
