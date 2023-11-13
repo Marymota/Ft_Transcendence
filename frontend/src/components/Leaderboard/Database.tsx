fc5784723d8b3c74a7258d1fd77cd892a971f559
Marymota <dg.marciamota@gmail.com>
2023-11-13 20:32:31 +0000

    chamged fonts using google fonts api

--- frontend/src/components/Leaderboard/Database.tsx
+++ frontend/src/components/Leaderboard/Database.tsx
@@ -13,5 +13,19 @@ export const DataBoard = [
         img: "https://avatars.githubusercontent.com/u/20130249?v=4",
         dt: "2023-09-09"
     },
+    {
+        name: "Craig",
+        location: "Portugal",
+        score: "1609",
+        img: "https://avatars.githubusercontent.com/u/20130249?v=4",
+        dt: "2023-11-09"
+    },
+    {
+        name: "Julio",
+        location: "Brasil",
+        score: "986",
+        img: "https://avatars.githubusercontent.com/u/20130249?v=4",
+        dt: "2023-09-09"
+    },
 ]
 
