ad5ccf07fb3a176ecbdf5c4274c42f4b57431a55
António Abranches Pinto <antoniomariabranches@gmail.com>
2023-11-21 11:26:19 +0000

    fixed the issue with channel entity repository - backend has no errors when executing

--- backend/src/modules/chat.module.ts
+++ backend/src/modules/chat.module.ts
@@ -4,9 +4,11 @@ import { UserModule } from './user.module';
 import { UserService } from 'src/services/user.service';
 import { UserController } from 'src/controllers/user.controller';
 import { ChatService } from 'src/services/chat.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Channel from 'src/entitys/channel.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forFeature([Channel]), UserModule],
   providers: [ChatGateway, UserService, ChatService],
   controllers: [UserController],
   exports: [ChatService],
